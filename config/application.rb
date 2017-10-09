ff4ad66d3ec8d1dc715301690e2d52df66576b1d
pcliffordsf <paulclifford2570@gmail.com>
2017-10-08 19:10:10 -0700

    more config bs

--- config/application.rb
+++ config/application.rb
@@ -20,8 +20,9 @@ Bundler.require(*Rails.groups)
 module Cloudperfect
   class Application < Rails::Application
     config.active_record.schema_format = :sql
-    config.assets.precompile += Ckeditor.assets
-    config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    config.assets.enabled = true
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w(ckeditor/*)
   end
 end
